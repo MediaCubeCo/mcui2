bdce1f2be984dc674b7a340907056edc082f29e6
Sadouski Denis <dsa@mediacube.network>
2026-02-02 16:24:17 +0100

    fix sanitize for data attrs

--- src/utils/sanitize.ts
+++ src/utils/sanitize.ts
@@ -2,25 +2,35 @@ import xss from 'xss'
 
 // more https://jsxss.com/en/index.html
 
+const whiteList = {
+  a: ['href', 'title', 'target', 'rel', 'data-*'],
+  b: [],
+  i: [],
+  em: [],
+  strong: [],
+  p: [],
+  br: [],
+  ul: [],
+  ol: [],
+  li: [],
+  div: ['data-*'],
+  span: ['style'],
+  img: ['src', 'alt', 'title', 'width', 'height'],
+}
+
 const sanitize = (html: string) => {
   return xss(html, {
-    whiteList: {
-      a: ['href', 'title', 'target', 'rel'],
-      b: [],
-      i: [],
-      em: [],
-      strong: [],
-      p: [],
-      br: [],
-      ul: [],
-      ol: [],
-      li: [],
-      div: [],
-      span: ['style'],
-      img: ['src', 'alt', 'title', 'width', 'height'],
-    },
+    whiteList,
+    allowList: {},
     stripIgnoreTag: true,
     stripIgnoreTagBody: ['script'],
+    onIgnoreTagAttr(tag, name, value, isWhiteAttr) {
+      //@ts-ignore
+      if (whiteList[tag] && whiteList[tag].includes('data-*') && name && name.startsWith('data-')) {
+        //@ts-ignore
+        return `${name}="${xss.escapeAttrValue(value)}"`
+      }
+    }
   })
 }
 
