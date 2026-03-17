68c03a88776bb9948911b34e02399f5c7fa0aec9
Sadouski Denis <dsa@mediacube.network>
2026-03-17 10:16:53 +0100

    fix datepicker month-picker disable dates

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,10 @@
-export * from './dayjs'
-export * from './sanitize'
-export * from './proxy'
-export * from './iconNameToComponentName'
+import { dayjs, dayjsLocales, setDayjsLocale } from './dayjs'
+import { sanitize } from './sanitize'
+import { createProxy } from './proxy'
+import { iconNameToComponentName } from './iconNameToComponentName'
 export * as mcGridColAdaptiveProps from './mcGridColAdaptiveProps'
 export * as mcSvgIconAdaptiveProps from './mcSvgIconAdaptiveProps'
 export * as mcTitleAdaptiveProps from './mcTitleAdaptiveProps'
+
+
+export { dayjs, dayjsLocales, setDayjsLocale, sanitize, createProxy, iconNameToComponentName }
