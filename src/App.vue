b994be59a5b8d6ab7bfbfb832ed055562bea07bc
roma.dziukau <rdz@mediacube.io>
2026-07-15 15:53:49 +0300

    CU-869e4j720 - fix

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
-<script setup lang="ts">
-import McFieldText from '@/components/elements/McFieldText/McFieldText.vue'
-import { ref } from 'vue'
-const value = ref(0)
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
-    <mc-field-text v-model="value" name="test" type="num" allow-negative />
   </div>
 </template>
 
